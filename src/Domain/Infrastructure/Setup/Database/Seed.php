03517e4bae80942c8ceaee238472912e06338857
Vadim <samokhinvadim@gmail.com>
2021-07-20 17:56:08 +0300

    User registration works

--- src/Domain/Infrastructure/Setup/Database/Seed.php
+++ src/Domain/Infrastructure/Setup/Database/Seed.php
@@ -12,12 +12,14 @@ use RC\Domain\Experience\Pure\LessThanAYear;
 use RC\Domain\Position\Pure\Analyst;
 use RC\Domain\Position\Pure\ProductDesigner;
 use RC\Domain\Position\Pure\ProductManager;
+use RC\Domain\UserProfileRecordType\Pure\Experience;
 use RC\Domain\UserProfileRecordType\Pure\Position;
 use RC\Infrastructure\ImpureInteractions\ImpureValue;
 use RC\Infrastructure\ImpureInteractions\ImpureValue\Successful;
 use RC\Infrastructure\ImpureInteractions\PureValue\Emptie;
 use RC\Infrastructure\SqlDatabase\Agnostic\OpenConnection;
 use RC\Infrastructure\SqlDatabase\Agnostic\Query\SingleMutating;
+use RC\Infrastructure\SqlDatabase\Agnostic\Query\TransactionalQueryFromMultipleQueries;
 use RC\Infrastructure\Uuid\RandomUUID;
 
 class Seed
@@ -80,9 +82,19 @@ class Seed
     private function addQuestions()
     {
         return
-            (new SingleMutating(
-                'insert into registration_question (id, profile_record_type, bot_id, ordinal_number, text) values (?, ?, ?, ?, ?)',
-                [Uuid::uuid4()->toString(), (new Position())->value(), '1f6d0fd5-3179-47fb-b92d-f6bec4e8f016', 1, 'Кем работаете?'],
+            (new TransactionalQueryFromMultipleQueries(
+                [
+                    new SingleMutating(
+                        'insert into registration_question (id, profile_record_type, bot_id, ordinal_number, text) values (?, ?, ?, ?, ?)',
+                        [Uuid::uuid4()->toString(), (new Position())->value(), '1f6d0fd5-3179-47fb-b92d-f6bec4e8f016', 1, 'Кем вы работаете?'],
+                        $this->connection
+                    ),
+                    new SingleMutating(
+                        'insert into registration_question (id, profile_record_type, bot_id, ordinal_number, text) values (?, ?, ?, ?, ?)',
+                        [Uuid::uuid4()->toString(), (new Experience())->value(), '1f6d0fd5-3179-47fb-b92d-f6bec4e8f016', 1, 'Какой у вас опыт?'],
+                        $this->connection
+                    ),
+                ],
                 $this->connection
             ))
                 ->response();
