c6e0a4195f565a6f7238f05310c68b56247045f3
Vadim <samokhinvadim@gmail.com>
2021-09-23 21:18:10 +0300

    add default value to sorry_is_sent column

---
+++ migrations/rc/20210923210000_add_default_false_value_to_sorry_is_sent_column_in_dropout_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddDefaultFalseValueToSorryIsSentColumnInDropoutTable extends AbstractMigration
+{
+    public function change()
+    {
+        $this->execute(
+            <<<q
+alter table meeting_round_dropout
+    add column sorry_is_sent set default false;
+q
+        );
+    }
+}
